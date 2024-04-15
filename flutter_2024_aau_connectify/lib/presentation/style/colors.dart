29191e2a0ab4b9462668af755d47c90a3d47840e
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-04-16 02:10:25 +0300

    defined custom colors and typography

---
+++ flutter_2024_aau_connectify/lib/presentation/style/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class CustomColors {
+  static const Color primaryColor = Color(0xFF53E8C4);
+  static const Color secondaryColor = Color(0xFFDA6317);
+  static const Color primaryTextColor = Color(0xFF53E8C4);
+  static const Color secondaryTextColor = Color(0xFF09051C);
+}
---
+++ flutter_2024_aau_connectify/lib/presentation/style/typography.dart
@@ -0,0 +1,50 @@
+
+
+import 'dart:ui';
+
+import 'package:flutter_2024_aau_connectify/presentation/style/colors.dart';
+
+class CustomTypography {
+  static TextStyle h1_black = TextStyle(
+    fontSize: 64,
+    fontWeight: FontWeight.w600,
+    color: CustomColors.secondaryTextColor,
+  );
+
+  static TextStyle h2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static  TextStyle h3 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle h4 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle h5 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle h6 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle body1 = TextStyle(
+    fontSize: 14,
+  );
+
+  static TextStyle body2 = TextStyle(
+    fontSize: 12,
+  );
+
+  static TextStyle caption = TextStyle(
+    fontSize: 10,
+  );
+}
