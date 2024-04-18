55a548d5352b04254c10245c740a1893aff13a82
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-04-19 01:26:31 +0300

    added custom standard heights and modified login page

--- flutter_2024_aau_connectify/lib/presentation/screens/login_page.dart
+++ flutter_2024_aau_connectify/lib/presentation/screens/login_page.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_2024_aau_connectify/presentation/style/colors.dart';
+import 'package:flutter_2024_aau_connectify/presentation/style/heights.dart';
+import 'package:flutter_2024_aau_connectify/presentation/style/paddings.dart';
 import 'package:flutter_2024_aau_connectify/presentation/style/radiuses.dart';
+import 'package:flutter_2024_aau_connectify/presentation/style/widths.dart';
+import 'package:json_theme/json_theme.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,16 +19,83 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    TextTheme _textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      backgroundColor: CustomColors.backgroundColor,
       body: SafeArea(
         child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
           Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(top: CustomPaddings.large),
             child: Column(
               children: [
-                CircleAvatar(
-                  radius: CustomRadius.extraUltraLarge,
-                  backgroundImage: AssetImage('assets/images/aau_logo.png'),
+                Padding(
+                  padding: EdgeInsets.only(bottom: CustomPaddings.small),
+                  child: CircleAvatar(
+                    backgroundColor: CustomColors.backgroundColor,
+                    radius: CustomRadius.ultimateLarge,
+                    backgroundImage: AssetImage('assets/images/aau_logo.png'),
+                  ),
+                ),
+                Text(
+                  "AAU Connectify",
+                  style: _textTheme.headlineSmall,
+                ),
+                SizedBox(
+                  height: CustomPaddings.extraUltraLarge,
+                ),
+                Text(
+                  "Login To Your Account",
+                  style: _textTheme.titleLarge,
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(CustomPaddings.large),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Username",
+                                  contentPadding:
+                                      EdgeInsets.all(CustomPaddings.small)),
+                            ),
+                            SizedBox(
+                              height: CustomPaddings.medium,
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Password",
+                                  suffixIcon: Icon(Icons.visibility),
+                                  contentPadding:
+                                      EdgeInsets.all(CustomPaddings.small)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Forgot Password?",
+                          style: _textTheme.button!.copyWith(
+                            color: CustomColors.primaryColor,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize:
+                              Size(ButtonWidths.large, ButtonHeights.large),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Login",
+                          style: _textTheme.button,
+                        ),
+                      ),
+                    ],
+                  ),
                 )
               ],
             ),
---
+++ flutter_2024_aau_connectify/lib/presentation/style/heights.dart
@@ -0,0 +1,7 @@
+
+
+class ButtonHeights {
+  static const double small = 40.0;
+  static const double medium = 48.0;
+  static const double large = 56.0;
+}
