4125f95d2da23ed6ff2bf4e02aa2c2ba2be64569
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-04-18 23:19:33 +0300

    started login page

--- flutter_2024_aau_connectify/lib/presentation/screens/login_page.dart
+++ flutter_2024_aau_connectify/lib/presentation/screens/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_2024_aau_connectify/presentation/style/colors.dart';
+import 'package:flutter_2024_aau_connectify/presentation/style/radiuses.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +12,22 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: CustomColors.backgroundColor,
+      body: SafeArea(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Container(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: CustomRadius.extraUltraLarge,
+                  backgroundImage: AssetImage('assets/images/aau_logo.png'),
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
