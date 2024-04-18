0ea187044c156ab1eaeae674e5a5ede4567622b3
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-04-18 14:45:57 +0300

    added additional pages

--- flutter_2024_aau_connectify/lib/presentation/screens/signup_page.dart
+++ flutter_2024_aau_connectify/lib/presentation/screens/signup_page.dart
@@ -10,6 +10,9 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Center(
+      child: Text('Sign Up Page'),
+    ));
   }
 }
