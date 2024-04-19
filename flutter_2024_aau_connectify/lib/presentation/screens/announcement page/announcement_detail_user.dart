fb8d6968034248305511f478b5716663c328d6f8
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-04-19 22:15:34 +0300

    started working on announcement detial page

---
+++ flutter_2024_aau_connectify/lib/presentation/screens/announcement page/announcement_detail_user.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class AnnouncementDetailUser extends StatefulWidget {
+  const AnnouncementDetailUser({super.key});
+
+  @override
+  State<AnnouncementDetailUser> createState() => _AnnouncementDetailUserState();
+}
+
+class _AnnouncementDetailUserState extends State<AnnouncementDetailUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
