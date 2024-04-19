3675ee2c7f405e189b5bfd93e841fb8f20159fc4
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-04-19 04:12:46 +0300

    finished reset password page

--- flutter_2024_aau_connectify/lib/presentation/navigation/route.dart
+++ flutter_2024_aau_connectify/lib/presentation/navigation/route.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_2024_aau_connectify/presentation/screens/home_page.dart';
 import 'package:flutter_2024_aau_connectify/presentation/screens/landing_page.dart';
 import 'package:flutter_2024_aau_connectify/presentation/screens/login_page.dart';
+import 'package:flutter_2024_aau_connectify/presentation/screens/reset_password/reset_password.dart';
+import 'package:flutter_2024_aau_connectify/presentation/screens/reset_password/reset_password_2.dart';
 
-import 'package:flutter_2024_aau_connectify/presentation/screens/signup_page.dart';
+import 'package:flutter_2024_aau_connectify/presentation/screens/sign_up_page/signup_page.dart';
+import 'package:flutter_2024_aau_connectify/presentation/screens/sign_up_page/signup_page_2.dart';
 
 const String loginRoute = '/login_page';
 const String homeRoute = '/home_page';
 const String signupRoute = '/signup_page';
 const String signupRoute2 = '/signup_page2';
 const String landingpageRoute = '/landingpage_page';
+const String announcementUser = '/Announcement_user_page';
+const String resetPasswordRoute = '/reset_password_page';
+const String resetPasswordRoute2 = '/reset_password_page2';
 
 Route<dynamic> controller(RouteSettings settings) {
   switch (settings.name) {
@@ -21,6 +27,14 @@ Route<dynamic> controller(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const LoginPage());
     case signupRoute:
       return MaterialPageRoute(builder: (context) => const SignUp());
+    case signupRoute2:
+      return MaterialPageRoute(builder: (context) =>  SignUp2());
+    case announcementUser:
+      return MaterialPageRoute(builder: (context) =>  const AnnouncementUserPage());
+    case resetPasswordRoute:
+      return MaterialPageRoute(builder: (context) =>  const ResetPasswordPage());
+    case resetPasswordRoute2:
+      return MaterialPageRoute(builder: (context) =>  const ResetPasswordPage2());
 
     default:
       return MaterialPageRoute(
