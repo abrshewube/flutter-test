709f9ead1ffb41eabdeeeb6ae843308e7ae35af3
jankoabel <jankoabel2022@gmail.com>
2024-06-19 09:49:20 -0700

    done

--- flutter_2024_aau_connectify/lib/main.dart
+++ flutter_2024_aau_connectify/lib/main.dart
@@ -1,39 +1,35 @@
-import 'package:flutter_2024_aau_connectify/presentation/navigation/route.dart'
-    as route;
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:json_theme/json_theme.dart';
-
-import 'package:flutter/services.dart'; // For rootBundle
-import 'dart:convert'; // For jsonDecode
+import 'package:flutter_2024_aau_connectify/presentation/navigation/route.dart' as route; // Adjust path as needed
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final themeStr =
-      await rootBundle.loadString('assets/theme/custom_theme.json');
+  // Load custom theme data from JSON
+  final themeStr = await rootBundle.loadString('assets/theme/custom_theme.json');
   final themeJson = jsonDecode(themeStr);
   final theme = ThemeDecoder.decodeThemeData(themeJson)!;
 
-  runApp(AAUConnectifyApp(theme: theme));
+  runApp(ProviderScope(
+    child: AAUConnectifyApp(theme: theme),
+  ));
 }
-
 
 class AAUConnectifyApp extends StatelessWidget {
   final ThemeData theme;
-  
-  
 
-  const AAUConnectifyApp({super.key, required this.theme});
+  const AAUConnectifyApp({Key? key, required this.theme}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      onGenerateRoute: route.controller,
-      initialRoute: route.landingpageRoute,
+      onGenerateRoute: route.controller, // Assuming route.controller generates routes correctly
+      initialRoute: route.landingpageRoute, // Assuming route.landingpageRoute is defined correctly
       theme: theme,
-      
     );
   }
 }
--- flutter_2024_aau_connectify/lib/presentation/screens/sign_up_page/signup_page.dart
+++ flutter_2024_aau_connectify/lib/presentation/screens/sign_up_page/signup_page.dart
@@ -1,165 +1,216 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_2024_aau_connectify/presentation/style/radiuses.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_2024_aau_connectify/presentation/style/colors.dart';
 import 'package:flutter_2024_aau_connectify/presentation/style/paddings.dart';
-import 'package:flutter_2024_aau_connectify/presentation/style/radiuses.dart';
-import 'package:flutter_2024_aau_connectify/presentation/style/typography.dart';
-import 'package:flutter_2024_aau_connectify/presentation/navigation/route.dart'
-    as route;
+import 'package:flutter_2024_aau_connectify/presentation/navigation/route.dart' as route;
+import 'package:flutter_2024_aau_connectify/presentation/service/api_service.dart';
 
-class SignUp extends StatefulWidget {
-  const SignUp({super.key});
+final authServiceProvider = Provider<AuthService>((ref) => AuthService());
+
+class SignUp extends ConsumerStatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
 
   @override
-  State<SignUp> createState() => _SignUpState();
+  ConsumerState<SignUp> createState() => _SignUpState();
 }
 
-void main() {
-  runApp(const SignUp());
-}
-
-class _SignUpState extends State<SignUp> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _studentPasswordController =
-      TextEditingController();
+class _SignUpState extends ConsumerState<SignUp> {
   final TextEditingController _idController = TextEditingController();
+  final TextEditingController _studentPasswordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _verificationCodeController = TextEditingController();
+
+  int _currentStep = 1;
 
   @override
   Widget build(BuildContext context) {
-    final customtext = Theme.of(context).textTheme;
+    final customText = Theme.of(context).textTheme;
+
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(bottom: CustomPaddings.small),
-            child: CircleAvatar(
-              backgroundColor: CustomColors.backgroundColor,
-              radius: CustomRadius.ultimateLarge,
-              backgroundImage: AssetImage('assets/images/aau_logo.png'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(bottom: CustomPaddings.small),
+              child: CircleAvatar(
+                backgroundColor: CustomColors.backgroundColor,
+                radius: CustomRadius.ultimateLarge,
+                backgroundImage: AssetImage('assets/images/aau_logo.png'),
+              ),
             ),
-          ),
-          Text(
-            "AAU Connectify",
-            style: customtext.headlineSmall,
-          ),
-          const SizedBox(
-            height: CustomPaddings.extraLarge,
-          ),
+            Text(
+              "AAU Connectify",
+              style: customText.headlineSmall,
+            ),
+            const SizedBox(
+              height: CustomPaddings.extraLarge,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: CustomPaddings.medium),
+                child: ListView(
+                  children: _buildStepContent(customText),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildStepContent(TextTheme customText) {
+    switch (_currentStep) {
+      case 1:
+        return [
           Container(
             margin: const EdgeInsets.symmetric(vertical: CustomPaddings.small),
             child: Text(
-              "Sign Up",
-              style: customtext.titleLarge,
+              "Step 1: Verify Student",
+              style: customText.headlineMedium, // Adjust text style as needed
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: CustomPaddings.medium),
-              child: ListView(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: CustomPaddings.small),
-                    child: TextField(
-                      decoration: const InputDecoration(
-                          contentPadding:
-                              EdgeInsets.only(left: CustomPaddings.medium),
-                          label: Text('Name')),
-                      controller: _nameController,
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: CustomPaddings.small),
-                    child: TextField(
-                      decoration: const InputDecoration(
-                          contentPadding:
-                              EdgeInsets.only(left: CustomPaddings.medium),
-                          label: Text('ID')),
-                      controller: _idController,
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: CustomPaddings.small),
-                    child: TextField(
-                      decoration: const InputDecoration(
-                          contentPadding:
-                              EdgeInsets.only(left: CustomPaddings.medium),
-                          label: Text('Student Password')),
-                      controller: _studentPasswordController,
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: CustomPaddings.small),
-                    child: TextField(
-                      decoration: const InputDecoration(
-                          contentPadding:
-                              EdgeInsets.only(left: CustomPaddings.medium),
-                          label: Text('User Name')),
-                      controller: _usernameController,
-                    ),
-                  )
-                ],
-              ),
+          _buildTextField('ID', _idController),
+          _buildTextField('Student Password', _studentPasswordController),
+          _buildButton('Verify', _verifyStudent),
+        ];
+      case 2:
+        return [
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: CustomPaddings.small),
+            child: Text(
+              "Step 2: Register User",
+              style: customText.headlineMedium, // Adjust text style as needed
             ),
           ),
-          Column(
-            children: [
-              FractionallySizedBox(
-                widthFactor: 0.8,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Navigator.pop(context);
-                    Navigator.pushNamed(context, route.signupRoute2);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: CustomColors.primaryColor,
-                    foregroundColor: CustomColors.backgroundColor,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 20),
-                    textStyle: const TextStyle(
-                      fontFamily: FontFamily.primary,
-                      fontSize: CustomFontSize.h4,
-                      fontWeight: FontWeight.bold,
-                      color: CustomColors.backgroundColor,
-                    ),
-                  ),
-                  child: const Text('Create Account'),
-                ),
-              ),
-              const SizedBox(
-                height: CustomPaddings.small,
-              ),
-              // Registration option
-              Padding(
-                padding:
-                    const EdgeInsets.only(bottom: CustomPaddings.ultraLarge),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Already have an account?"),
-                    TextButton(
-                        onPressed: () =>
-                            (Navigator.pushNamed(context, route.loginRoute)),
-                        child: const Text(
-                          "Login",
-                          style: TextStyle(
-                              color: CustomColors.primaryColor,
-                              fontSize: CustomFontSize.h6,
-                              fontWeight: FontWeight.bold),
-                        ))
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
+          _buildTextField('Name', _nameController),
+          _buildTextField('Email', _emailController),
+          _buildTextField('Password', _passwordController),
+          _buildButton('Register', _registerUser),
+        ];
+      case 3:
+        return [
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: CustomPaddings.small),
+            child: Text(
+              "Step 3: Verify Email",
+              style: customText.headlineMedium, // Adjust text style as needed
+            ),
+          ),
+          _buildTextField('Verification Code', _verificationCodeController),
+          _buildButton('Verify', _verifyEmail),
+        ];
+      default:
+        return [];
+    }
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: CustomPaddings.small),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.only(left: CustomPaddings.medium),
+          labelText: label,
+        ),
+        controller: controller,
       ),
-    ));
+    );
+  }
+
+  Widget _buildButton(String text, VoidCallback onPressed) {
+    return FractionallySizedBox(
+      widthFactor: 0.8,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: CustomColors.primaryColor,
+          foregroundColor: CustomColors.backgroundColor,
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          textStyle: const TextStyle(
+            fontSize: 18, // Adjust text style as needed
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+
+Future<void> _verifyStudent() async {
+  final studentId = _idController.text;
+  final studentPassword = _studentPasswordController.text;
+
+  print('Verifying student with ID: $studentId');
+
+  try {
+    final authService = ref.read(authServiceProvider);
+    final studentExists = await authService.checkStudentExistence(studentId, studentPassword);
+
+    print('Student exists result: $studentExists');
+
+    if (studentExists) {
+      setState(() {
+        _currentStep = 2;
+      });
+    } else {
+      _showErrorMessage('Student verification failed.');
+    }
+  } catch (e) {
+    print('Error in _verifyStudent: $e');
+    _showErrorMessage('Error: $e');
+  }
+}
+
+
+  Future<void> _registerUser() async {
+    final fullName = _nameController.text;
+    final email = _emailController.text;
+    final password = _passwordController.text;
+    final studentId = _idController.text;
+    final studentPassword = _studentPasswordController.text;
+
+    try {
+      final authService = ref.read(authServiceProvider); // Accessing authServiceProvider using ref.read
+      final success = await authService.registerUser(fullName, email, password, studentId, studentPassword);
+
+      if (success) {
+        setState(() {
+          _currentStep = 3;
+        });
+      } else {
+        _showErrorMessage('User registration failed.');
+      }
+    } catch (e) {
+      _showErrorMessage('Error: $e');
+    }
+  }
+
+  Future<void> _verifyEmail() async {
+    final email = _emailController.text;
+    final code = _verificationCodeController.text;
+
+    try {
+      final authService = ref.read(authServiceProvider); // Accessing authServiceProvider using ref.read
+      final success = await authService.verifyEmail(email, code);
+
+      if (success) {
+        Navigator.pushNamed(context, route.homeRoute); // Assuming route.homeRoute is correctly defined
+      } else {
+        _showErrorMessage('Email verification failed.');
+      }
+    } catch (e) {
+      _showErrorMessage('Error: $e');
+    }
+  }
+
+  void _showErrorMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
   }
 }
